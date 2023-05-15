1e456f522f2f41e59025885b2995bd49e6d4c7ae
Dales10 <matiasdario75@gmail.com>
2023-05-15 16:03:50 -0300

    Using docker-compose and changing the SQLite database to PostgreSQL.

--- prisma/migrations/20230515185316_init/migration.sql
+++ prisma/migrations/20230515185316_init/migration.sql
@@ -1,18 +1,20 @@
 -- CreateTable
 CREATE TABLE "user" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "firstName" TEXT NOT NULL,
     "lastName" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "tel" TEXT NOT NULL,
     "password" TEXT NOT NULL,
-    "date" DATETIME NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
     "city" TEXT NOT NULL,
     "cep" TEXT NOT NULL,
     "country" TEXT NOT NULL,
     "state" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
